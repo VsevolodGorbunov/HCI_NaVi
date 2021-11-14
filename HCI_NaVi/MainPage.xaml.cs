e291a2babf918b2102fae071b7baecfd14d572ce
VsevolodGorbunov <gorbunovvo@mer.ci.nsu.ru>
2021-11-14 22:46:38 +0700

    Add project files.

---
+++ HCI_NaVi/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HCI_NaVi
+{
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e) //Обработчик нажатия на кнопку
+        {
+
+        }
+
+    }
+}
+
